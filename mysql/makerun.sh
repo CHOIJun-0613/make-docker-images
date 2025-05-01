f8a1494c897e489fa1ceab659d8c6225ead3ee0e
cjonyou <cjonyou@naver.com>
2025-05-01 22:17:32 +0900

    수정정

---
+++ mysql/makerun.sh
@@ -0,0 +1,6 @@
+
+
+docker build --no-cache -t dev-mysql-db:latest .
+
+
+docker run -d --name dev-mysql-db -e MYSQL_ROOT_PASSWORD=#skcc06433 -e MYSQL_USER=devuser -e MYSQL_PASSWORD=#skcc06433 -e MYSQL_DATABASE=devdb -p 3306:3306  -v mysql_data:/var/lib/mysql  dev-mysql-db
